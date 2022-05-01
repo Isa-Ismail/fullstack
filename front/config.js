34062066ff715f09801cdf049965c9aabd66bab4
Fahim_tech_support <fahimismail75.scc@gmail.com>
2022-05-01 14:18:11 +0600

    'localhost'

--- front/config.js
+++ front/config.js
@@ -2,6 +2,6 @@ import getConfig from 'next/config'
 
 const { publicRuntimeConfig } = getConfig()
 
-export const API = publicRuntimeConfig.API ? 'https://seoblog.com' : 'https://localhost:8000'
+export const API = publicRuntimeConfig.API ? 'https://seoblog.com' : 'http://localhost:8000'
 
 export const APP_NAME = publicRuntimeConfig.APP_NAME
